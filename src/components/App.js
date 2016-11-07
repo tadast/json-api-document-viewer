539e7f448bbc6a9f44e14b6b5c49775fbe75a03c
Tadas Tamosauskas <tadas@pdfcv.com>
2016-11-07 20:14:04 +0000

    Init {json:api} document viewer
    Naive implementation of {json:api} document viewer. It does not handle any edge
    cases (it perhaps loops infinitely if there's a circular relationship) and does
    not resolve linked relationships.

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router';
 import { version } from '../../package.json';
+import Entity from './Entity';
+import JsonApiDataStore from 'jsonapi-datastore';
 
-const App = ({ children }) => (
-  <div>
-    <header>
-      <h1>React Starterify {version}</h1>
-      <Link to="/about">About</Link>
-      <Link to="/poweredby">Powered by</Link>
-    </header>
-    <section>
-      {children || 'Welcome to React Starterify'}
-    </section>
-  </div>
-);
+class App extends React.Component {
+  componentWillMount() {
+    const store = new JsonApiDataStore.JsonApiDataStore();
+    this.setState({
+      store: store.sync(this.props.doc)
+    });
+  }
 
-App.propTypes = { children: React.PropTypes.object };
+  entities() {
+    if (this.state.store instanceof Array) {
+      return this.state.store;
+    }
+    return [this.state.store];
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>json:api document viewer {version}</h1>
+        </header>
+        <section>
+          {this.entities().map((entity, idx) => (<Entity key={idx} data={entity} />))}
+        </section>
+      </div>
+    );
+  }
+
+}
+
+App.propTypes = { doc: React.PropTypes.object.isRequired };
 
 export default App;
